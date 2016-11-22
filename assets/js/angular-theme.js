e258a333aa94416dc1349e7218f3a6ab3733cae9
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2016-11-21 23:36:09 -0500

    Creating an AngularJS Factory

--- assets/js/angular-theme.js
+++ assets/js/angular-theme.js
@@ -1,1 +1,7 @@
-var wpApp = new angular.module('wpAngularTheme', ['ui.router']);
+var wpApp = new angular.module('wpAngularTheme', ['ui.router', 'ngResource']);
+
+wpApp.factory('Post', function($resource){
+	return $resource( appInfo.api_url + 'posts/:ID', {
+		ID: '@id'
+	})
+});
