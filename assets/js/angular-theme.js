4981019092f4a4715f1021b32ac5f5b4c7ba7c34
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2016-11-22 01:37:18 -0500

    Creating a single post template and route

--- assets/js/angular-theme.js
+++ assets/js/angular-theme.js
@@ -1,31 +1,45 @@
-var wpApp = new angular.module('wpAngularTheme', ['ui.router', 'ngResource']);
+var wpApp = new angular.module( 'wpAngularTheme', ['ui.router', 'ngResource'] );
 
-wpApp.factory('Posts', function($resource){
+wpApp.factory( 'Posts', function( $resource ) {
 	return $resource( appInfo.api_url + 'posts/:ID', {
 		ID: '@id'
 	})
 });
 
-wpApp.controller('ListCtrl', ['$scope', 'Posts', function ($scope, Posts){
+wpApp.controller( 'ListCtrl', ['$scope', 'Posts', function( $scope, Posts ) {
 	console.log('ListCtrl');
-	$scope.page_title = "Blog Listing Page";
-	Posts.query(function(res){
+	$scope.page_title = 'Blog Listing Page';
+
+	Posts.query(function( res ) {
 		$scope.posts = res;
-	})
+	});
+	
 }]);
+
+wpApp.controller( 'DetailCtrl', ['$scope', '$stateParams', 'Posts', function( $scope, $stateParams, Posts ) {
+	console.log( $stateParams );
+	Posts.query( {id: $stateParams.id }, function(res){
+		$scope.post = res[0];
+	})
+}])
 
 wpApp.config( function( $stateProvider, $urlRouterProvider){
 	$urlRouterProvider.otherwise('/');
 	$stateProvider
-	.state( 'list', {
-		url: '/',
-		controller: 'ListCtrl',
-		templateUrl: appInfo.template_directory + 'templates/list.html'
-	})
+		.state( 'list', {
+			url: '/',
+			controller: 'ListCtrl',
+			templateUrl: appInfo.template_directory + 'templates/list.html'
+		})
+		.state( 'detail',{
+			url: '/posts/:id',
+			controller: 'DetailCtrl',
+			templateUrl: appInfo.template_directory + 'templates/detail.html'
+		})
 });
 
-wpApp.filter('to_trusted', ['$sce', function($sce){
-	return function( text ){
+wpApp.filter( 'to_trusted', ['$sce', function( $sce ){
+	return function( text ) {
 		return $sce.trustAsHtml( text );
 	}
-}]);
+}])
